f4c22f0cf787393a06a6c00bdea63580b511b75b
yakujaae-afk <yakujaae@gmail.com>
2026-03-23 19:55:23 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
+api_id = 30296254
+api_hash = "c2b5306f4ccd2d795405a026c10b4c62"
+bot_token = "7732080124:AAF-qB0ROba9miDGPCCToN4rtWC9IQrv9Gc"
+auth_users = [766091689]
 sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+log_channel = -1001235155926
+txt_channel = -1001235155926
 
 
