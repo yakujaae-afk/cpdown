81271928ac98785380ae7ccff4eb9eab5f710add
yakujaae-afk <yakujaae@gmail.com>
2026-04-12 06:20:22 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 30296254
 api_hash = "c2b5306f4ccd2d795405a026c10b4c62"
-bot_token = "7732080124:AAF-qB0ROba9miDGPCCToN4rtWC9IQrv9Gc"
+bot_token = "8729794086:AAGImqYUrPm86zftTZ1Q9-JK09FhGOf8ymo"
 auth_users = [766091689]
 sudo_user = 1713924419
 log_channel = -1001235155926
