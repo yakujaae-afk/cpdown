fd3293095a43e0aa4415d85bca1f0a5632cdbfb4
yakujaae-afk <yakujaae@gmail.com>
2026-04-18 22:31:24 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,5 @@
-api_id = 30296254
-api_hash = "c2b5306f4ccd2d795405a026c10b4c62"
-bot_token = "8729794086:AAGImqYUrPm86zftTZ1Q9-JK09FhGOf8ymo"
-auth_users = [766091689]
-sudo_user = 1713924419
-log_channel = -1001235155926
-txt_channel = -1001235155926
+import os
 
-
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
